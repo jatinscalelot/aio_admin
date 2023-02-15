e72a4a9f6803d5ba08acee15b04afc05e331ae46
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-02-16 04:43:44 +0530

    initial commit

--- controllers/roles/getone.js
+++ controllers/roles/getone.js
@@ -1,7 +1,20 @@
 const roleModel = require('../../models/roles.model');
-const adminModel = require('../../models/admins.model');
 const responseManager = require('../../utilities/response.manager');
 const mongoConnection = require('../../utilities/connections');
 const constants = require('../../utilities/constants');
 const mongoose = require('mongoose');
-exports.getone = async (req, res) => {};
+exports.getone = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.token.adminid && mongoose.Types.ObjectId.isValid(req.token.adminid)) {
+        const { roleid } = req.body;
+        if(roleid && roleid != '' && mongoose.Types.ObjectId.isValid(roleid)){
+            let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+            let roledata = await primary.model(constants.MODELS.roles, roleModel).findById(roleid).lean();
+            return responseManager.onSuccess("Roles data...", roledata, res);
+        }else{
+            return responseManager.badrequest({ message: 'Invalid role id to get role data, please try again' }, res);
+        }
+    }else{
+        return responseManager.badrequest({ message: 'Invalid token to get role data, please try again' }, res);
+    }
+};
--- controllers/roles/list.js
+++ controllers/roles/list.js
@@ -1,7 +1,28 @@
 const roleModel = require('../../models/roles.model');
-const adminModel = require('../../models/admins.model');
 const responseManager = require('../../utilities/response.manager');
 const mongoConnection = require('../../utilities/connections');
 const constants = require('../../utilities/constants');
 const mongoose = require('mongoose');
-exports.list = async (req, res) => {};
+exports.list = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.token.adminid && mongoose.Types.ObjectId.isValid(req.token.adminid)) {
+        const { page, limit, search } = req.body;
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        primary.model(constants.MODELS.roles, roleModel).paginate({
+            $or: [
+                { name : { '$regex' : new RegExp(search, "i") } }
+            ],
+        },{
+            page,
+            limit: parseInt(limit),
+            sort: { _id : -1 },
+            lean: true
+        }).then((roleslist) => {
+            return responseManager.onSuccess("Roles list...", roleslist, res);
+        }).catch((error) => {
+            return responseManager.onError(error, res);
+        });
+    }else{
+        return responseManager.badrequest({ message: 'Invalid token to get roles list, please try again' }, res);
+    }
+};
