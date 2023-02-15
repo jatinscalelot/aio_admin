c36bcbdc6ad284f84f9785bdfb46a3fac82ac08f
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-02-16 04:28:56 +0530

    initial commit

---
+++ controllers/roles/list.js
@@ -0,0 +1,7 @@
+const roleModel = require('../../models/roles.model');
+const adminModel = require('../../models/admins.model');
+const responseManager = require('../../utilities/response.manager');
+const mongoConnection = require('../../utilities/connections');
+const constants = require('../../utilities/constants');
+const mongoose = require('mongoose');
+exports.list = async (req, res) => {};
