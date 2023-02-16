16872ae5225df6eef32689a74c5a826d72ff29f5
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-02-17 00:52:59 +0530

    update permission model and authonticater code

--- models/admins.model.js
+++ models/admins.model.js
@@ -1,38 +1,42 @@
 let mongoose = require("mongoose");
 let mongoosePaginate = require("mongoose-paginate-v2");
 let schema = new mongoose.Schema({
-    name : {
-        type : String,
-        require : true
+    name: {
+        type: String,
+        require: true
     },
-    email : {
-        type : String,
-        require : true
+    email: {
+        type: String,
+        require: true
     },
-    mobile : {
-        type : String,
-        require : true
+    mobile: {
+        type: String,
+        require: true
     },
-    country_code : {
-        type : String,
-        require : true
+    country_code: {
+        type: String,
+        require: true
     },
-    status : {
+    status: {
         type: Boolean,
-        require : true
+        require: true
     },
-    password : {
-        type : String,
-        require : true
+    password: {
+        type: String,
+        require: true
     },
-	createdBy: {
-		type: mongoose.Types.ObjectId,
-		default: null
-	},
-	updatedBy: {
-		type: mongoose.Types.ObjectId,
-		default: null
-	}
+    role: {
+        type: mongoose.Types.ObjectId,
+        require: true
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        default: null
+    },
+    updatedBy: {
+        type: mongoose.Types.ObjectId,
+        default: null
+    }
 }, { timestamps: true, strict: false, autoIndex: true });
 schema.plugin(mongoosePaginate);
 module.exports = schema;
--- routes/register.js
+++ routes/register.js
@@ -6,31 +6,32 @@ const constants = require('../utilities/constants');
 const helper = require('../utilities/helper');
 const adminModel = require('../models/admins.model');
 router.post('/', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    const { name, email, mobile, country_code, password } = req.body;
-    if(name && name.trim() != '' && email && email.trim() != '' && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) && mobile && mobile.length == 10 && country_code && country_code.trim() != '' && password && password.length >= 6){
+    const { name, email, mobile, country_code, password, role } = req.body;
+    if (name && name.trim() != '' && email && email.trim() != '' && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) && mobile && mobile.length == 10 && country_code && country_code.trim() != '' && password && password.length >= 6 && role && role != '' && mongoose.Types.ObjectId.isValid(role)) {
         let ecnPassword = await helper.passwordEncryptor(password);
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-        let checkExisting = await primary.model(constants.MODELS.admins, adminModel).findOne({ $or: [ {mobile: mobile}, {email: email} ] }).lean();
-        if(checkExisting == null){
+        let checkExisting = await primary.model(constants.MODELS.admins, adminModel).findOne({ $or: [{ mobile: mobile }, { email: email }] }).lean();
+        if (checkExisting == null) {
             let obj = {
-                name : name,
-                email : email,
-                mobile : mobile,
-                country_code : country_code,
-                password : ecnPassword,
-                status : true,
-                createdBy : null,
-                updatedBy : null
+                name: name,
+                email: email,
+                mobile: mobile,
+                country_code: country_code,
+                password: ecnPassword,
+                status: true,
+                role: mongoose.Types.ObjectId(role),
+                createdBy: null,
+                updatedBy: null
             };
-            adminData = await primary.model(constants.MODELS.admins, adminModel).create(obj);
+            let adminData = await primary.model(constants.MODELS.admins, adminModel).create(obj);
             return responseManager.onSuccess('Admin register successfully!', adminData, res);
-        }else{
+        } else {
             return responseManager.onSuccess('Admin with same email or mobile already exist, please try with another one!', 0, res);
         }
-    }else{
-        return responseManager.badrequest({message : 'Invalid data to register admin, Please try again...'}, res);
+    } else {
+        return responseManager.badrequest({ message: 'Invalid data to register admin, Please try again...' }, res);
     }
 });
 module.exports = router;
